c2bbff7d9da1bee99215e02c1aadbf8f9f759523
wdajun <51494794@qq.com>
2019-08-17 17:03:20 +0800

    add check page

--- src/app/pages/pack-check/pack-check.page.ts
+++ src/app/pages/pack-check/pack-check.page.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PackCheckPage implements OnInit {
 
+  public checkpoints: Array<{}> = [];
+  public selectedHours: Number = 0;
+
   constructor() { }
 
   ngOnInit() {
+    this.checkpoints = [
+      {
+        title : '发动机',
+        expand : false,
+        points : [
+          {label : '发动机问题1', key : "00001", selected : false},
+          {label : '发动机问题2', key : "00002", selected : false},
+          {label : '发动机问题3', key : "00003", selected : false},
+          {label : '发动机问题4', key : "00004", selected : false},
+        ]
+      },
+      {
+        title : '异响',
+        expand : false,
+        points : [
+          {label : '异响问题1', key : "00011", selected : false},
+          {label : '异响问题2', key : "00012", selected : false},
+          {label : '异响问题3', key : "00013", selected : false},
+          {label : '异响问题4', key : "00014", selected : false},
+        ]
+      },
+
+    ]
+  }
+
+  doExpand(event,item) {
+    item.expand = !item.expand;
+    event.stopPropagation();
+    
   }
 
 }
