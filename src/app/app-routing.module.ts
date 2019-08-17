878ff4bb9e2f261bd8a7f5f28f93004122d91e2c
Neil Sun <liangscc@163.com>
2019-08-17 10:10:02 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
-  { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
-  { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
-  { path: 'edit-profile', loadChildren: './pages/edit-profile/edit-profile.module#EditProfilePageModule' },
-  { path: 'home-results', loadChildren: './pages/home-results/home-results.module#HomeResultsPageModule' },
+  { path: 'app/', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'app/register', loadChildren: './pages/register/register.module#RegisterPageModule' },
+  { path: 'app/about', loadChildren: './pages/about/about.module#AboutPageModule' },
+  { path: 'app/settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
+  { path: 'app/edit-profile', loadChildren: './pages/edit-profile/edit-profile.module#EditProfilePageModule' },
+  { path: 'app/home-results', loadChildren: './pages/home-results/home-results.module#HomeResultsPageModule' },
 ];
 
 @NgModule({
