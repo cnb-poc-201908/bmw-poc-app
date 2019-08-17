e7f0ed0cf65e90b9ffb02799e3754b1b0fd43e18
Neil Sun <liangscc@163.com>
2019-08-17 10:55:18 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'app/', loadChildren: './pages/login/login.module#LoginPageModule' },
-  // { path: 'app/about', loadChildren: './pages/about/about.module#AboutPageModule' },
-  // { path: 'app/settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
-  // { path: 'app/edit-profile', loadChildren: './pages/edit-profile/edit-profile.module#EditProfilePageModule' },
-  { path: 'app/home-results', loadChildren: './pages/home-results/home-results.module#HomeResultsPageModule' },
-  { path: 'app/packages', loadChildren: './pages/packages/packages.module#PackagesPageModule' },
-  { path: 'app/virecle-list', loadChildren: './pages/virecle-list/virecle-list.module#VirecleListPageModule' },
-  { path: 'app/search-filter', loadChildren: './pages/modal/search-filter/search-filter.module#SearchFilterPageModule' },
+  { path: '', loadChildren: './pages/login/login.module#LoginPageModule' },
+  // { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
+  // { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
+  // { path: 'edit-profile', loadChildren: './pages/edit-profile/edit-profile.module#EditProfilePageModule' },
+  { path: 'home-results', loadChildren: './pages/home-results/home-results.module#HomeResultsPageModule' },
+  { path: 'packages', loadChildren: './pages/packages/packages.module#PackagesPageModule' },
+  { path: 'virecle-list', loadChildren: './pages/virecle-list/virecle-list.module#VirecleListPageModule' },
+  { path: 'search-filter', loadChildren: './pages/modal/search-filter/search-filter.module#SearchFilterPageModule' },
 
 ];
 
