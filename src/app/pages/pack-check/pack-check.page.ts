b83c864762e7a1d8f1b246f9cd874653a5309d7c
wdajun <51494794@qq.com>
2019-08-19 11:16:49 +0800

    pack check changes

--- src/app/pages/pack-check/pack-check.page.ts
+++ src/app/pages/pack-check/pack-check.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-pack-check',
@@ -8,12 +9,123 @@ import { AlertController } from '@ionic/angular';
 })
 export class PackCheckPage implements OnInit {
 
-  public checkpoints: Array<{}> = [];
-  public selectedHours: Number = 0;
+  public categories = [];
+  public content = [];
+  public checkpoints = [];
+  public selectedPoints = [];
+  public selectedHours = 0;
 
-  constructor(private alertCtrl: AlertController) { }
+  constructor(private alertCtrl: AlertController,
+    private rest: RestService) { }
 
   ngOnInit() {
+    this.content = [{
+      "CHETNO": "A",
+      "CHETDES": "驾驶反馈",
+      "BRNO": "1",
+      "BRDES": "发动机问题",
+      "ASPAKNO": "CHE01",
+      "ASPAKDES": "发动机问题:"
+     },
+     {
+      "CHETNO": "A",
+      "CHETDES": "驾驶反馈",
+      "BRNO": "2",
+      "BRDES": "仪表盘问题",
+      "ASPAKNO": "CHE02",
+      "ASPAKDES": "仪表盘问题:"
+     },
+     {
+      "CHETNO": "A",
+      "CHETDES": "驾驶反馈",
+      "BRNO": "3",
+      "BRDES": "车身抖动",
+      "ASPAKNO": "CHE03",
+      "ASPAKDES": "车身抖动:"
+     },
+     {
+      "CHETNO": "A",
+      "CHETDES": "驾驶反馈",
+      "BRNO": "4",
+      "BRDES": "刹车问题",
+      "ASPAKNO": "CHE04",
+      "ASPAKDES": "刹车问题:"
+     },
+     {
+      "CHETNO": "A",
+      "CHETDES": "驾驶反馈",
+      "BRNO": "5",
+      "BRDES": "驾驶问题",
+      "ASPAKNO": "CHE05",
+      "ASPAKDES": "驾驶问题:"
+     },
+     {
+      "CHETNO": "B",
+      "CHETDES": "乘坐反馈",
+      "BRNO": "1",
+      "BRDES": "车内噪音大",
+      "ASPAKNO": "CHE06",
+      "ASPAKDES": "车内噪音大:"
+     },
+     {
+      "CHETNO": "B",
+      "CHETDES": "乘坐反馈",
+      "BRNO": "2",
+      "BRDES": "胎噪大",
+      "ASPAKNO": "CHE07",
+      "ASPAKDES": "胎噪大:"
+     },
+     {
+      "CHETNO": "B",
+      "CHETDES": "乘坐反馈",
+      "BRNO": "3",
+      "BRDES": "座椅问题",
+      "ASPAKNO": "CHE08",
+      "ASPAKDES": "座椅问题:"
+     },
+     {
+      "CHETNO": "B",
+      "CHETDES": "乘坐反馈",
+      "BRNO": "4",
+      "BRDES": "车内空调问题",
+      "ASPAKNO": "CHE09",
+      "ASPAKDES": "车内空调问题:"
+     },
+     {
+      "CHETNO": "B",
+      "CHETDES": "乘坐反馈",
+      "BRNO": "5",
+      "BRDES": "车内娱乐系统问题",
+      "ASPAKNO": "CHE10",
+      "ASPAKDES": "车内娱乐系统问题:"
+     },
+     {
+      "CHETNO": "C",
+      "CHETDES": "其他反馈",
+      "BRNO": "1",
+      "BRDES": "外观问题",
+      "ASPAKNO": "CHE11",
+      "ASPAKDES": "外观问题:"
+     },
+     {
+      "CHETNO": "C",
+      "CHETDES": "其他反馈",
+      "BRNO": "2",
+      "BRDES": "内饰问题",
+      "ASPAKNO": "CHE12",
+      "ASPAKDES": "内饰问题:"
+     },
+     {
+      "CHETNO": "C",
+      "CHETDES": "其他反馈",
+      "BRNO": "3",
+      "BRDES": "互联驾驶问题",
+      "ASPAKNO": "CHE13",
+      "ASPAKDES": "互联驾驶问题:"
+     }
+    ]
+   
+    /** 
     this.checkpoints = [
       {
         title : '发动机',
@@ -37,6 +149,48 @@ export class PackCheckPage implements OnInit {
       },
 
     ]
+    */
+
+   this.rest.getPackageList().subscribe(res=>{
+    if (res && res.code === 200) {
+      const packs = res.basicInfoList;
+      let result = packs.filter(item=>{
+        return item.RepairTypeCode === 'CHE'
+      })
+
+      let sets = new Set();
+      this.content.forEach(item=>{
+        sets.add(item.CHETDES)
+      })
+
+      let temp = Array.from(sets) 
+      temp.forEach(item=>{
+        let obj = { title : item, expand : false, points : []}
+        this.content.forEach(con=>{
+          if (item === con.CHETDES) {
+            obj.points.push({ label : con.ASPAKDES, key : con.ASPAKNO, selected: false})
+          }
+        })
+        this.checkpoints.push(obj)
+      })
+
+      this.checkpoints.forEach(item=>{
+        item.points.forEach(pt=>{
+          result.forEach(res=>{
+            if (pt.key === res.PackageID) {
+              pt.hours = Number(res.Laborinfo[0].LaborAmount)
+            }
+          })
+        })
+      })
+
+      console.log(this.checkpoints)
+
+      
+    }
+  })
+
+
   }
 
   doExpand(event,item) {
